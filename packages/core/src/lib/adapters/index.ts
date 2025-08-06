f1e4b424f7bdd12c9c4c49d85a79a924df22acda
fermeridamagni <hello@magni.dev>
2025-08-05 23:10:31 -0600

    refactor: remove unused adapter-related code and schemas

--- packages/core/src/lib/adapters/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Adapter, AdapterConfig } from '@/types/adapters';
-
-function createAdapter(config: AdapterConfig): Adapter {
-  return {
-    id: config.id,
-  };
-}
-
-export default createAdapter;
--- packages/core/src/types/adapters.d.ts
+++
@@ -1,6 +0,0 @@
-import type { adapterConfigSchema, adapterSchema } from '@schemas/adaper';
-import type { z } from 'zod';
-
-export interface Adapter extends z.infer<typeof adapterSchema> {}
-
-export interface AdapterConfig extends z.infer<typeof adapterConfigSchema> {}
