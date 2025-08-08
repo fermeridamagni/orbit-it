9f355087801a5d32396836fa2add2f563c8c4a81
fermeridamagni <hello@magni.dev>
2025-08-07 20:58:30 -0600

    fix: update test include path in vitest configuration for consistency

--- packages/orbit-it/vitest.config.ts
+++ packages/orbit-it/vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    include: ['src/tests/**/*.{test}.ts'],
+    include: ['tests/**/*.{test}.ts'],
     exclude: ['node_modules', 'dist', '.turbo'],
     alias,
   },
